7530e859f6a6bce7575f24a65e113881d1653cda
Anil Raj <anilraj659@gmail.com>
2017-02-09 03:23:19 +0530

    creating STN app v1

---
+++ boot.ts
@@ -0,0 +1,12 @@
+// The browser platform with a compiler
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// The app module
+import { AppModule } from './app/app.module';
+import {enableProdMode} from '@angular/core';
+
+enableProdMode();
+
+// Compile and launch the module
+platformBrowserDynamic().bootstrapModule(AppModule);
